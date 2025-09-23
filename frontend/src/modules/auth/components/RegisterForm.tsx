113640b1d9e878c1725986512fb3d62e106f69ff
eedu7 <mueedahmad067@gmail.com>
2025-09-23 11:26:28 +0500

    feat(auth): add RegisterForm with react-hook-form and zod validation
    - Implemented controlled form using shadcn/ui form components
    - Added email and password fields with validation via zodResolver
    - Included submit handler with console logging for form values

---
+++ frontend/src/modules/auth/components/RegisterForm.tsx
@@ -0,0 +1,62 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+	email: z.email(),
+	password: z.string(),
+});
+
+export const RegisterForm = () => {
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			email: "",
+			password: "",
+		},
+	});
+
+	const onSubmit = (values: z.infer<typeof formSchema>) => {
+		console.log(values);
+	};
+
+	return (
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+				<FormField
+					control={form.control}
+					name="email"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Email</FormLabel>
+							<FormControl>
+								<Input placeholder="e.g, example@example.example" {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name="password"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Password</FormLabel>
+							<FormControl>
+								<Input placeholder="e.g, Password@123" {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<Button type="submit" className="w-full">
+					Register
+				</Button>
+			</form>
+		</Form>
+	);
+};
