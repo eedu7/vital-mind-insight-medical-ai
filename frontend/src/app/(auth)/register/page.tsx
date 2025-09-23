c72378584cd614e4481f4029f2970b9432fd7f39
eedu7 <mueedahmad067@gmail.com>
2025-09-23 11:26:42 +0500

    feat(auth): add registration page with RegisterForm and social sign-in options
    - Added page metadata for SEO and app branding
    - Integrated RegisterForm inside shadcn/ui Card layout
    - Added social login buttons (Google, Facebook, Apple) with icons
    - Included link to login page for existing users

---
+++ frontend/src/app/(auth)/register/page.tsx
@@ -0,0 +1,56 @@
+import { RegisterForm } from "@/modules/auth/components";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { IconBrandApple, IconBrandFacebook, IconBrandGoogle } from "@tabler/icons-react";
+import { Metadata } from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+	title: "Register - VitalMind Insight",
+	description: "Register - VitalMind Insight Health AI",
+};
+
+export default async function page() {
+	return (
+		<div className="flex h-full flex-col justify-between">
+			<div>
+				<h1>VitalMind Insight Health AI</h1>
+			</div>
+			<Card>
+				<CardHeader>
+					<CardDescription>Start you journey</CardDescription>
+					<CardTitle>Sign Up to VitalMind Insight</CardTitle>
+				</CardHeader>
+				<CardContent>
+					<RegisterForm />
+				</CardContent>
+				<CardFooter className="flex flex-col gap-y-4">
+					<div className="flex w-full items-center gap-4">
+						<Separator className="flex-1" />
+						<span className="text-muted-foreground text-sm">or sign up with</span>
+						<Separator className="flex-1" />
+					</div>
+
+					<div className="space-x-4">
+						<Button variant="outline" size="icon" className="cursor-pointer">
+							<IconBrandGoogle />
+						</Button>
+						<Button variant="outline" size="icon" className="cursor-pointer">
+							<IconBrandFacebook />
+						</Button>
+						<Button variant="outline" size="icon" className="cursor-pointer">
+							<IconBrandApple />
+						</Button>
+					</div>
+				</CardFooter>
+			</Card>
+			<div className="flex gap-x-1">
+				<p className="text-sm">Have an account?</p>
+				<Link href="/login" className="text-sm text-blue-500 hover:underline">
+					Sign in
+				</Link>
+			</div>
+		</div>
+	);
+}
