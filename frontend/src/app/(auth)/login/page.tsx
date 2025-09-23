3ef17ec435b90ea79c26520a552e83862e46aba4
eedu7 <mueedahmad067@gmail.com>
2025-09-23 11:26:57 +0500

    feat(auth): add basic login page with metadata
    - Created login page with placeholder content
    - Added page metadata (title and description)

---
+++ frontend/src/app/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Login",
+	description: "Login Page",
+};
+
+export default function page() {
+	return <div className="">Login Page</div>;
+}
