01d4013c313de50d9fd1c483bcca62ec55814a53
eedu7 <mueedahmad067@gmail.com>
2025-09-28 21:56:40 +0500

    Added some settings

--- frontend/src/app/(home)/@conversations/page.tsx
+++ frontend/src/app/(home)/@conversations/page.tsx
@@ -1,49 +1,37 @@
+"use client";
 import { Button } from "@/components/ui/button";
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import { Textarea } from "@/components/ui/textarea";
 import { IconSparkles } from "@tabler/icons-react";
+import { useRouter } from "next/navigation";
+import { ChangeEvent, FormEvent, useState } from "react";
 
 export default function ConversationPageView() {
+	const router = useRouter();
+	const [prompt, setPrompt] = useState("");
+	const onSubmit = async (e: FormEvent) => {
+		e.preventDefault();
+		e.stopPropagation();
+
+		
+		
+	};
+
 	return (
 		<div className="mx-auto flex h-full max-w-4xl flex-col items-center justify-center py-14">
-			<div className="space-y-14">
-				<div className="flex flex-wrap gap-4">
-					<Card className="min-w-[250px] flex-1">
-						<CardHeader>
-							<CardTitle>Role - 1</CardTitle>
-						</CardHeader>
-					</Card>
-					<Card className="min-w-[250px] flex-1">
-						<CardHeader>
-							<CardTitle>Role - 2</CardTitle>
-						</CardHeader>
-					</Card>
-					<Card className="min-w-[250px] flex-1">
-						<CardHeader>
-							<CardTitle>Role - 3</CardTitle>
-						</CardHeader>
-					</Card>
-					<Card className="min-w-[250px] flex-1">
-						<CardHeader>
-							<CardTitle>Role - 4</CardTitle>
-						</CardHeader>
-					</Card>
-					<Card className="min-w-[250px] flex-1">
-						<CardHeader>
-							<CardTitle>Role - 5</CardTitle>
-						</CardHeader>
-					</Card>
-				</div>
-				<div>
-					<form className="space-y-2">
-						<Textarea className="h-44 resize-none" placeholder="Add some prompt" />
-						<div className="text-end">
-							<Button variant="outline" size="icon" className="cursor-pointer">
-								<IconSparkles />
-							</Button>
-						</div>
-					</form>
-				</div>
+			<div className="space-y-4">
+				<h1 className="font-dancing-script text-shadow-xl text-center text-2xl font-bold">What is on your mind</h1>
+				<form onSubmit={onSubmit} className="space-y-2">
+					<Textarea
+						className="max-h-44 w-xl max-w-xl resize-y"
+						placeholder="Add some prompt"
+						onChange={(e: ChangeEvent<HTMLTextAreaElement>) => setPrompt(e.target.value)}
+					/>
+					<div className="text-end">
+						<Button variant="outline" size="icon" className="cursor-pointer">
+							<IconSparkles />
+						</Button>
+					</div>
+				</form>
 			</div>
 		</div>
 	);
