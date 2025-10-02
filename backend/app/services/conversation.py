ba2df35ae17025ebf260b35c57c82cd14cdbdeec
eedu7 <mueedahmad067@gmail.com>
2025-10-02 14:01:10 +0500

    feat(conversation): add service methods for fetching conversations
    - Added `get_all_conversations` in ConversationService to fetch all conversations by user_id
    - Added `get_by_uuid` to fetch a specific conversation by UUID and user_id
    - Improved error handling with 400/404 responses

--- backend/app/services/conversation.py
+++ backend/app/services/conversation.py
@@ -1,3 +1,6 @@
+from typing import List, cast
+from uuid import UUID
+
 from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -18,3 +21,33 @@ class ConversationService:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST, detail=f"Error on creating conversation: {exc}"
             )
+
+    async def get_all_conversations(self, user_id: int, session: AsyncSession) -> List[Conversation]:
+        try:
+            conversations = await self.repository.get_all_conversations(user_id=user_id, session=session)
+
+        except Exception as exc:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, detail=f"Error on fetching conversation by id: {exc}"
+            )
+
+        if not conversations:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail=f"Error conversation with uuid {uuid} not found."
+            )
+        return cast(List[Conversation], conversations)
+
+    async def get_by_uuid(self, uuid: UUID, user_id: int, session: AsyncSession) -> List[Conversation]:
+        try:
+            conversations = await self.repository.get_by_uuid(uuid=uuid, user_id=user_id, session=session)
+
+        except Exception as exc:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, detail=f"Error on fetching conversation by id: {exc}"
+            )
+
+        if not conversations:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail=f"Error conversation with uuid {uuid} not found."
+            )
+        return cast(List[Conversation], conversations)
